c002fe58951628632c1aedc94d4b28ca6747cf79
Jose Meireles <jmeirele@c1r2s6.42porto.com>
2024-11-12 11:11:00 +0000

    mandaroty part done, fixed save remaining

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:55:54 by jmeirele          #+#    #+#             */
-/*   Updated: 2024/11/11 18:11:20 by jmeirele         ###   ########.fr       */
+/*   Updated: 2024/11/12 11:09:39 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ char	*ft_read_to_new_line(int fd, char *storage)
 		bytes_read = read(fd, temp, BUFFER_SIZE);
 		if (bytes_read == -1)
 			return (free(temp), free(storage), NULL);
+		if (bytes_read == 0)
+			break;
 		temp[bytes_read] = '\0';
 		storage = ft_strjoin(storage, temp);
 	}
@@ -61,56 +63,18 @@ char	*ft_save_remaining(char *storage)
 {
 	char	*to_save;
 	size_t	len;
-	size_t	i;
-
-	len = 0;
-	i = 0;
-	if (!storage)
-		return (NULL);
-	while (storage[len] && storage[len] != '\n')
-		len++;
-	if (!storage[len])  // If no newline is found, free storage and return NULL
-	{
-		free(storage);
-		return (NULL);
-	}
-	i = ft_strlen(storage);
-	to_save = malloc(i - len);
-	if (!to_save)
-		return (NULL);
-	i = 0;
-	len++;
-	while (storage[len])
-	{
-		to_save[i] = storage[len];
-		i++;
-		len++;
-	}
-	to_save[i] = '\0';
-	free(storage);
-	storage = NULL;
-	return (to_save);
-}
-/* char	*ft_save_remaining(char *storage)
-{
-	char	*to_save;
-	size_t	len;
 
 	len = 0;
 	if (!storage)
 		return (NULL);
 	while (storage[len] && storage[len] != '\n')
 		len++;
-	if (!storage[len])  // If no newline is found, free storage and return NULL
-	{
-		free(storage);
-		return (NULL);
-	}
+	if (!storage[len])
+		return (free(storage), NULL);
 	to_save = ft_strdup(storage + len + 1);
 	free(storage);
 	return (to_save);
-} */
-
+}
 
 char	*get_next_line(int fd)
 {
@@ -120,30 +84,28 @@ char	*get_next_line(int fd)
 	if (!storage)
 	{
 		storage = malloc(1);
-		storage[0] = '\0';
 		if (!storage)
-		{
-			free(storage);
-			return (NULL);
-		}
+			return (free(storage), NULL);
+		storage[0] = '\0';
 	}
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	storage = ft_read_to_new_line(fd, storage);
-	if (!storage)
-		return (free(storage), storage = NULL, NULL);
 	res = ft_extract_line(storage);
 	storage = ft_save_remaining(storage);
 	return (res);
 }
 
-// int	main(void)
-// {
-// 	char	*res;
-// 	int	fd = open("tests/test3.txt", O_RDONLY);
-// 	while ((res = get_next_line(fd)))
-// 	{
-// 		printf("%s", res);
-// 		free(res);
-// 	}
-// }
+/* int	main(void)
+{
+	char	*res;
+	int	fd = open("tests/test3.txt", O_RDONLY);
+	// res = get_next_line(fd);
+	// printf("%s", res);
+	// free(res);
+	while ((res = get_next_line(fd)))
+	{
+		printf("%s", res);
+		free(res);
+	}
+} */
